aaa1bc9caaefc5fef14a6038e9ac4973ff946411
l17011117 <pierre.lejeune@etu.univ-amu.fr>
2018-12-05 10:44:45 +0100

    Ajout FXML, Maven architecture

--- src/main/java/fr/univamu/iut/marche/traitement/Main.java
+++ src/main/java/fr/univamu/iut/marche/traitement/Main.java
@@ -6,11 +6,26 @@ import fr.univamu.iut.marche.traitement.acteurs.Paysan;
 import fr.univamu.iut.marche.traitement.produits.Cochon;
 import fr.univamu.iut.marche.traitement.produits.Laitage;
 import fr.univamu.iut.marche.traitement.produits.ProduitFermier;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/views/sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
     public static void main(String[] args) {
 
         ArrayList pro = new ArrayList();
@@ -21,5 +36,7 @@ public class Main {
         Paysan p1 = new Apiculteur("Oui", "Non", 40, pro);
 
         p1.addProduit(new Cochon(5, "4"));
+        launch(args);
+        
     }
 }
--- src/main/java/fr/univamu/iut/marche/traitement/UniteDeProduction.java
+++ src/main/java/fr/univamu/iut/marche/traitement/UniteDeProduction.java
@@ -10,38 +10,91 @@ import java.util.*;
 public  class UniteDeProduction {
     public ProduitFermier cree(String type, Paysan P)throws ClassNotFoundException{
         String prof = P.getClass().getSimpleName();
-        switch (type){
-            case "orange":
-                if(prof.equals("Orticulteur"))return new Lait(5,calcDatePremption());
-            case "pomme":
-                if(prof.equals("Orticulteur"))return new Lait(5,calcDatePremption());
-                else {
-                    System.out.println("Vous ne pouvez pas produire ceci");
-                }
-            case "lait":
-                if(prof.equals("ProducteurLaitier"))return new Lait(5,calcDatePremption());
-                else {
-                    System.out.println("Vous ne pouvez pas produire ceci");
-                }
-            case "fromage":
-                if(prof.equals("ProducteurLaitier"))return new Fromage(5,calcDatePremption());
-                else {
-                    System.out.println("Vous ne pouvez pas produire ceci");
-                }
-            case "vache":
-                if(prof.equals("ProducteurDeViande"))return new Vache(5,calcDatePremption());
-                else {
-                    System.out.println("Vous ne pouvez pas produire ceci");
-                }
-                break;
-            case "cochon":
-                if(prof.equals("ProducteurDeViande"))return new Cochon(5,calcDatePremption());
-                else {
-                    System.out.println("Vous ne pouvez pas produire ceci");
-                }
-                break;
-            default:
-                throw new ClassNotFoundException();
+        if ("orange".equals(type)) {
+            if (prof.equals("Orticulteur")) return new Lait(5, calcDatePremption());
+
+            if (prof.equals("Orticulteur")) return new Lait(5, calcDatePremption());
+            else {
+                System.out.println("Vous ne pouvez pas produire ceci");
+            }
+
+            if (prof.equals("ProducteurLaitier")) return new Lait(5, calcDatePremption());
+            else {
+                System.out.println("Vous ne pouvez pas produire ceci");
+            }
+
+            if (prof.equals("ProducteurLaitier")) return new Fromage(5, calcDatePremption());
+            else {
+                System.out.println("Vous ne pouvez pas produire ceci");
+            }
+
+            if (prof.equals("ProducteurDeViande")) return new Vache(5, calcDatePremption());
+            else {
+                System.out.println("Vous ne pouvez pas produire ceci");
+            }
+
+        } else if ("pomme".equals(type)) {
+            if (prof.equals("Orticulteur")) return new Lait(5, calcDatePremption());
+            else {
+                System.out.println("Vous ne pouvez pas produire ceci");
+            }
+
+            if (prof.equals("ProducteurLaitier")) return new Lait(5, calcDatePremption());
+            else {
+                System.out.println("Vous ne pouvez pas produire ceci");
+            }
+
+            if (prof.equals("ProducteurLaitier")) return new Fromage(5, calcDatePremption());
+            else {
+                System.out.println("Vous ne pouvez pas produire ceci");
+            }
+
+            if (prof.equals("ProducteurDeViande")) return new Vache(5, calcDatePremption());
+            else {
+                System.out.println("Vous ne pouvez pas produire ceci");
+            }
+
+        } else if ("lait".equals(type)) {
+            if (prof.equals("ProducteurLaitier")) return new Lait(5, calcDatePremption());
+            else {
+                System.out.println("Vous ne pouvez pas produire ceci");
+            }
+
+            if (prof.equals("ProducteurLaitier")) return new Fromage(5, calcDatePremption());
+            else {
+                System.out.println("Vous ne pouvez pas produire ceci");
+            }
+
+            if (prof.equals("ProducteurDeViande")) return new Vache(5, calcDatePremption());
+            else {
+                System.out.println("Vous ne pouvez pas produire ceci");
+            }
+
+        } else if ("fromage".equals(type)) {
+            if (prof.equals("ProducteurLaitier")) return new Fromage(5, calcDatePremption());
+            else {
+                System.out.println("Vous ne pouvez pas produire ceci");
+            }
+
+            if (prof.equals("ProducteurDeViande")) return new Vache(5, calcDatePremption());
+            else {
+                System.out.println("Vous ne pouvez pas produire ceci");
+            }
+
+        } else if ("vache".equals(type)) {
+            if (prof.equals("ProducteurDeViande")) return new Vache(5, calcDatePremption());
+            else {
+                System.out.println("Vous ne pouvez pas produire ceci");
+            }
+
+        } else if ("cochon".equals(type)) {
+            if (prof.equals("ProducteurDeViande")) return new Cochon(5, calcDatePremption());
+            else {
+                System.out.println("Vous ne pouvez pas produire ceci");
+            }
+
+        } else {
+            throw new ClassNotFoundException();
         }
         return null;
     }
