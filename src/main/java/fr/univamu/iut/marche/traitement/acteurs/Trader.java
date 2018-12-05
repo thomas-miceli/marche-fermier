28397e4506279e7706f026e0dcfeb1061afaf419
Dylan March <37612877+dylan-march@users.noreply.github.com>
2018-12-05 12:02:27 +0100

    Ajout Trader pas fini

--- src/main/java/fr/univamu/iut/marche/traitement/acteurs/Trader.java
+++ src/main/java/fr/univamu/iut/marche/traitement/acteurs/Trader.java
@@ -1,11 +1,25 @@
 package fr.univamu.iut.marche.traitement.acteurs;
 
+import fr.univamu.iut.marche.traitement.CentraleAchat;
 import fr.univamu.iut.marche.traitement.acteurs.Participant;
+import fr.univamu.iut.marche.traitement.produits.ProduitFermier;
 
 import java.util.List;
 
 public class Trader extends Participant {
-    public Trader(String nom, String prenom, int age) {
+    private CentraleAchat centrale;
+
+    public Trader(String nom, String prenom, int age, CentraleAchat centrale)
+    {
         super(nom, prenom, age);
+        this.centrale = centrale;
+    }
+
+    public void Acheter(ProduitFermier produitFermier) {
+
+    }
+
+    public void Vendre(ProduitFermier produitFermier) {
+
     }
 }
