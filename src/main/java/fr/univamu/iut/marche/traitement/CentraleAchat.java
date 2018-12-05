69a47bc55ee50777b66f54d41e5232a4dcdbf51e
Andrea Garcia <37613221+andrea-garciaa@users.noreply.github.com>
2018-12-05 11:19:17 +0100

    centrale achat init

--- src/main/java/fr/univamu/iut/marche/traitement/CentraleAchat.java
+++ src/main/java/fr/univamu/iut/marche/traitement/CentraleAchat.java
@@ -1,4 +1,28 @@
 package fr.univamu.iut.marche.traitement;
 
+import fr.univamu.iut.marche.traitement.acteurs.Participant;
+import fr.univamu.iut.marche.traitement.acteurs.Paysan;
+import fr.univamu.iut.marche.traitement.acteurs.Trader;
+import fr.univamu.iut.marche.traitement.produits.ProduitFermier;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CentraleAchat {
+    Map<ProduitFermier, Integer> prixMoyen = new HashMap<>();
+
+    ArrayList<Trader> acheteurs = new ArrayList<>();
+
+    public CentraleAchat(Map<ProduitFermier, Integer> prixMoyen) {
+        this.prixMoyen = prixMoyen;
+    }
+
+    public void enregisterTrader(Trader trader) {
+        acheteurs.add(trader);
+    }
+
+    public void recalculerPrix(String classeDeProduit) {
+
+    }
 }
