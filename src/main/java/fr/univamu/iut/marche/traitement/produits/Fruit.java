bda9aba41395ea51d64edf210e84741b9d971a99
Dylan March <37612877+dylan-march@users.noreply.github.com>
2018-12-05 08:43:16 +0100

    Ajout de quelques classes

--- src/main/java/fr/univamu/iut/marche/traitement/produits/Fruit.java
+++ src/main/java/fr/univamu/iut/marche/traitement/produits/Fruit.java
@@ -1,4 +1,7 @@
 package fr.univamu.iut.marche.traitement.produits;
 
-public class Fruit {
+public class Fruit extends ProduitFermier {
+    public Fruit(int quantite, String dateDePeremption) {
+        super(quantite, dateDePeremption);
+    }
 }
