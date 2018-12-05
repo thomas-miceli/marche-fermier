c1e99eaf347996755889fbd293b876d1245d55c5
Téo Martin <37612909+teo-martin@users.noreply.github.com>
2018-12-05 10:01:57 +0100

    Add files via upload
    Début Implémentation classe Marche

--- src/main/java/fr/univamu/iut/marche/traitement/Marche.java
+++ src/main/java/fr/univamu/iut/marche/traitement/Marche.java
@@ -1,4 +1,30 @@
 package fr.univamu.iut.marche.traitement;
 
+import fr.univamu.iut.marche.traitement.acteurs.Paysan;
+import fr.univamu.iut.marche.traitement.produits.ProduitFermier;
+
+import java.util.ArrayList;
+import java.util.Map;
+
 public class Marche {
+    private Map<Paysan, ArrayList<ProduitFermier>> listeParticipantsMarche;
+
+    public Marche(Map<Paysan, ArrayList<ProduitFermier>> listeParticipantsMarche) {
+        this.listeParticipantsMarche = listeParticipantsMarche;
+    }
+
+    public Map<Paysan, ArrayList<ProduitFermier>> getListeParticipantsMarche() {
+        return listeParticipantsMarche;
+    }
+
+    public void setListeParticipantsMarche(Map<Paysan, ArrayList<ProduitFermier>> listeParticipantsMarche) {
+        this.listeParticipantsMarche = listeParticipantsMarche;
+
+    }
+
+    @Override
+    public String toString() {
+        return "liste des fermiers et des produits qu'ils vendent : "
+                + listeParticipantsMarche.get(listeParticipantsMarche);
+    }
 }
