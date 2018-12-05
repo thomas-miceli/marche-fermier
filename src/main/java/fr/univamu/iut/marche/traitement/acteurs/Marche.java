bb442f72f2368937a727922ef32fa428fe1a94fa
MARTIN Teo <m17015514@a-maq-iutinfo.salsa.univ-amu.fr>
2018-12-05 11:56:41 +0100

    Modification des classes Marche et Paysan

--- src/main/java/fr/univamu/iut/marche/traitement/acteurs/Marche.java
+++ src/main/java/fr/univamu/iut/marche/traitement/acteurs/Marche.java
@@ -4,10 +4,11 @@ import fr.univamu.iut.marche.traitement.acteurs.Paysan;
 import fr.univamu.iut.marche.traitement.produits.ProduitFermier;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Marche {
-    private Map<Paysan, ArrayList<ProduitFermier>> listeParticipantsMarche;
+    private Map<Paysan, ArrayList<ProduitFermier>> listeParticipantsMarche = new HashMap<>();
 
     public Marche() {
     }
@@ -21,14 +22,23 @@ public class Marche {
 
     }
 
-    @Override
-    public String toString() {
-        return "liste des fermiers et des produits qu'ils vendent : "
-                + listeParticipantsMarche.get(listeParticipantsMarche);
+    public void AddToList(Paysan peon, ProduitFermier produitAVendre){
+        peon.getProduitsVendus().add(produitAVendre);
+        System.out.println(peon);
+        System.out.println(peon.getProduitsVendus());
+
+        listeParticipantsMarche.put(peon, peon.getProduitsVendus());
+
+        System.out.println(listeParticipantsMarche.toString());
     }
 
-    public void AddToList(Paysan peon, ProduitFermier produitAVendre){
-        peon.getProduits().add(produitAVendre);
+    public void listeProduitsPaysans (Paysan peon){
+        for(Map.Entry<Paysan, ArrayList<ProduitFermier>> entry : listeParticipantsMarche.entrySet()) {
+            Paysan peonCherche = entry.getKey();
+            if (peonCherche == peon){
+                peonCherche.getProduits();
+           }
+        }
     }
 
     public void VendreProduit(Paysan peon, ProduitFermier produitVendu){
--- src/main/java/fr/univamu/iut/marche/traitement/acteurs/Paysan.java
+++ src/main/java/fr/univamu/iut/marche/traitement/acteurs/Paysan.java
@@ -7,22 +7,36 @@ import java.util.List;
 
 public class Paysan extends Participant {
 
-    private List produits = new ArrayList<ProduitFermier>();
+    private List produitsEnStock = new ArrayList<ProduitFermier>();
+
+    private ArrayList<ProduitFermier> produitsVendus = new ArrayList<ProduitFermier>();
+
+    public void setProduits(List produits) {
+        this.produitsEnStock = produits;
+    }
+
+    public ArrayList<ProduitFermier> getProduitsVendus() {
+        return produitsVendus;
+    }
+
+    public void setProduitsVendus(ArrayList produitsVendus) {
+        this.produitsVendus = produitsVendus;
+    }
 
     public Paysan(String nom, String prenom, int age, List produits) {
         super(nom, prenom, age);
-        this.produits = produits;
+        this.produitsEnStock = produits;
     }
 
     public List getProduits() {
-        return produits;
+        return produitsEnStock;
     }
 
     public void addProduit(ProduitFermier produit) {
-        this.produits.add(produit);
+        this.produitsEnStock.add(produit);
     }
 
     public void removeProduit(ProduitFermier produit) {
-        this.produits.remove(produit);
+        this.produitsEnStock.remove(produit);
     }
 }
