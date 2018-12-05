991b037fd95b1da9a86b969f2b913d9fe9dcf014
f17007588 <yann.forner@etu.univ-amu.fr>
2018-12-05 11:16:25 +0100

    ep 2 fin uniteProd

--- src/main/java/fr/univamu/iut/marche/traitement/ProduictionDeFruit.java
+++ src/main/java/fr/univamu/iut/marche/traitement/ProduictionDeFruit.java
@@ -3,13 +3,11 @@ package fr.univamu.iut.marche.traitement;
 import fr.univamu.iut.marche.traitement.acteurs.Paysan;
 import fr.univamu.iut.marche.traitement.produits.ProduitFermier;
 
-public class ProductionDeViande extends UniteDeProduction {
-
-
+public class ProduictionDeFruit extends UniteDeProduction {
     @Override
     public ProduitFermier fabriquer(String type, Paysan P) throws ClassNotFoundException {
         ProduitFermier prod= cree(type,P);
-        System.out.println("La production de viande a crée un "+prod.getNom()+" grace au paysan : "+ P.getNom());
+        System.out.println("La production de fruits a crée un "+prod.getNom()+" grace au paysan : "+ P.getNom());
         return prod;
     }
 }
