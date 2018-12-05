9d1ca5c9522349db5e2839e16ce922e6d891e1fe
Dylan March <37612877+dylan-march@users.noreply.github.com>
2018-12-05 08:59:01 +0100

    Add files via upload

---
+++ src/main/java/fr/univamu/iut/marche/traitement/produits/Miel.java
@@ -0,0 +1,6 @@
+package fr.univamu.iut.marche.traitement.produits;
+
+public class Miel extends ProduitFermier {
+    public Miel(int quantite, String dateDePeremption) { super(quantite, dateDePeremption);
+    }
+}
