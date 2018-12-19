92a731ee047b13df70738d52af18da7b8e992181
teo-martin <teo.martin@etu.univ-amu.fr>
2018-12-19 17:06:07 +0100

    Implémentation de TraderSpecial (expérimental)

--- src/main/java/fr/univamu/iut/marche/traitement/acteurs/TraderSpecial.java
+++ src/main/java/fr/univamu/iut/marche/traitement/acteurs/TraderSpecial.java
@@ -2,27 +2,74 @@ package fr.univamu.iut.marche.traitement.acteurs;
 
 import fr.univamu.iut.marche.traitement.produits.ProduitFermier;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
 import static fr.univamu.iut.marche.traitement.Main.ANSI_RED;
+import static fr.univamu.iut.marche.traitement.Seeding.compilerProduits;
 
 /**
  * @author Dylan MARCH
  * @author Téo MARTIN
  */
 public class TraderSpecial extends Trader {
-    public TraderSpecial(String nom, String prenom, int age) {
+    public TraderSpecial(String nom, String prenom, int age) throws ParseException {
         super(nom, prenom, age);
     }
+    private ArrayList<ProduitFermier> stockTrader;
+    private String dateToConvert="18:30:00";
+    private Date finDeService = new SimpleDateFormat("").parse(dateToConvert);
+    private Date dateActuelle = new Date();
 
-    @Override
-    public void vendreProduit(Participant participant, ProduitFermier produitAVendre, int Quantite) {
-
-        try{
-            produitAVendre.setQuantite(produitAVendre.getQuantite()-Quantite);
-            for (ProduitFermier produit:produitsEnStock) {
-                if(produit.getId()==produitAVendre.getId()) produit = produitAVendre;
+    public void vendreProduit(CentraleAchat centraleAchat, ProduitFermier produitAVendre) {
+        if (finDeService.after(dateActuelle)){
+            ProduitFermier aVendre = null;
+            for (ProduitFermier produ : produitsEnStock) {
+                if (produ.getClass().getSimpleName().toUpperCase().equals(produitAVendre.getClass().getSimpleName())) {
+                    aVendre = produ;
+                    break;
+                }
             }
-        }catch (Exception e){
-            System.out.println("un probleme est survenus");
+            this.getProduitsAVendre().remove(aVendre);
+        }
+        else {
+            ProduitFermier aVendre = null;
+            for (ProduitFermier produ : produitsEnStock) {
+                if (produ.getClass().getSimpleName().toUpperCase().equals(produitAVendre.getClass().getSimpleName())) {
+                    aVendre = produ;
+                    break;
+                }
+            }
+            centraleAchat.getStockAVendreCentrale().remove(aVendre);
+        }
+    }
+
+    public void acheterProduit(CentraleAchat centraleAchat, Participant vendeur, Produits produitAchete) {
+        if (finDeService.after(dateActuelle)){
+            ProduitFermier aAcheter = null;
+            for (ProduitFermier produ : produitsEnStock) {
+                if (produ.getClass().getSimpleName().toUpperCase().equals(produitAchete.getClass().getSimpleName())) {
+                    aAcheter = produ;
+                    break;
+                }
+            }
+            this.getProduits().add(aAcheter);
+            this.setProduits(compilerProduits(centraleAchat.getStockCentrale()));
+            vendeur.produitsEnVente.remove(aAcheter);
+        }
+        else {
+            ProduitFermier aAcheter = null;
+            for (ProduitFermier produ : produitsEnStock) {
+                if (produ.getClass().getSimpleName().toUpperCase().equals(produitAchete.getClass().getSimpleName())) {
+                    aAcheter = produ;
+                    break;
+                }
+            }
+            centraleAchat.getStockCentrale().add(aAcheter);
+            centraleAchat.setStockCentrale(compilerProduits(centraleAchat.getStockCentrale()));
+            vendeur.produitsEnVente.remove(aAcheter);
         }
     }
 }
