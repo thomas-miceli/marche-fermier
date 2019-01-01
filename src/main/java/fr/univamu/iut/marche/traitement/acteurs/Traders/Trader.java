12a847038ced15af41bbaf45a7ce10d2d26713e3
Forner Yann <37612939+Yann-Forner@users.noreply.github.com>
2019-01-01 20:47:09 +0100

    Add files via upload

--- src/main/java/fr/univamu/iut/marche/traitement/acteurs/Traders/Trader.java
+++ src/main/java/fr/univamu/iut/marche/traitement/acteurs/Traders/Trader.java
@@ -1,19 +1,44 @@
 package fr.univamu.iut.marche.traitement.acteurs.Traders;
 
+import fr.univamu.iut.marche.traitement.acteurs.Marche;
 import fr.univamu.iut.marche.traitement.acteurs.Participant;
-import fr.univamu.iut.marche.traitement.produits.ProduitFermier;
 
 import java.util.ArrayList;
 
-/**
- * @author Dylan MARCH
- * @author Téo MARTIN
- */
-public abstract class Trader extends Participant {
+public class Trader {
+    private String name;
+    private Double solde=0.0;
+    private ArrayList<Participant> clients = new ArrayList<>();
 
-    public Trader(String nom, String prenom, int age)
-    {
-        super(nom, prenom,age);
+    public Trader(String name) {
+        this.name = name;
+    }
+    public void addClients (Participant p){
+        clients.add(p);
+        p.setTrader(this);
+    }
+    public void metEnVente(Participant client, Participant.Produits produitMisEnVente , Integer quantite, Double prix, Marche marche){
+        client.vendreProduit(produitMisEnVente,quantite,prix,marche);
+    }
+    public void poseUneOffre(Participant client, Participant.Produits produitMisEnVente , Integer quantite, Double prix, Marche marche){
+        client.proposerOffre(produitMisEnVente,quantite,prix,marche);
+    }
+    public void ajouterAuSolde(Double revenu){
+        solde+=revenu*1/8;
+    }
+    public Double getSolde() {
+        return solde;
     }
 
+    public void setSolde(Double solde) {
+        this.solde = solde;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Participant> getClients() {
+        return clients;
+    }
 }
